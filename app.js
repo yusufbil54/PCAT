7f6f8fe87f92efd94a4870e44a10b61a435c1fe9
Yusuf Bil <104075393+yusufbil54@users.noreply.github.com>
2023-02-09 16:46:52 +0300

    MongoDb updated

--- app.js
+++ app.js
@@ -2,25 +2,38 @@ const express=require('express');
 const app=express();
 const ejs=require('ejs');
 const path=require('path');
+const Photo= require('./models/Photo')
+const mongoose = require('mongoose');
+//connection DB
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //TEMPLATE ENGINE 
 app.set('view engine', 'ejs');
 
-
-
 //MIDDLEWEARS
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
 
 //ROUTES
-app.get('/',(req,res)=>{
-    //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-    res.render('index');
+app.get('/', async (req,res)=>{
+    const photos = await Photo.find({});
+    res.render('index',{
+        photos
+    });
 });
 app.get('/about',(req,res)=>{
     res.render('about');
 });
 app.get('/add',(req,res)=>{
     res.render('add');
+});
+app.post('/photos', async (req,res)=>{
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 const port=3000;
